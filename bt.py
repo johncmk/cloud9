67bb6002138cddda7a915fa1aab595df5234345d
john <johnck408@gmail.com>
2016-04-18 00:56:01 +0000

    Binary Tree

---
+++ bt.py
@@ -0,0 +1,12 @@
+class Tree():
+
+    __slots__ = "root,left,right".split()
+
+    def __init__(self, root=None, left=None, right=None):
+        self.root = root
+        self.left = left
+        self.right = right
+        
+if __name__ == "__main__":
+    
+    print 5
