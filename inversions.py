95f92cbc5779eb2226c96a5f67d2b5f5784cd1fd
john <johnck408@gmail.com>
2016-04-30 13:40:30 +0000

    Largest number of inversion proof added

--- inversions.py
+++ inversions.py
@@ -1,16 +1,28 @@
-'''Find the number of inversion
+'''
+Find the number of inversion
+
 ex [3,1,4,2]
     1) 3,1
     2) 3,2
     3) 4,3
-    therefore the function returns 3 in the given case'''
+    therefore the function returns 3 in the given case
+
+the largest possible number of inversion can be found 
+in the followin formular
+
+n(n-1) / 2
+
+i.e. [6,5,4,3,2,1] has n = 6
+
+6(6-1) / 2  ->  6(5)/2  ->  30/2  -> 15 is the largest possible number of inversion.
+'''
     
     
-'''this is lazy solution
+'''this is naive solution
 For each element, count number of elements 
 which are on right side of it and are smaller than it.
 This bring solution however it's O(n)..'''    
-def inversions_lazy(li):
+def inversions_naive(li):
     
     if len(li) <= 1:
         return 0
@@ -26,11 +38,12 @@ def inversions_lazy(li):
 '''
 Optimal Solution will be using the mergesort
 such that each time you merge and find the left el is greater than
-the right array then increment the inverion count'''
+the right array then increment the inverion count. O(nlogn)
+'''
 
 # inversion = inv1 + inv2 + cross_inv
 
-def mergeSort(li,inv = 0):
+def inversions(li,inv = 0):
     
     if len(li) <= 1:
         return li,inv
@@ -39,11 +52,10 @@ def mergeSort(li,inv = 0):
     l = li[:mid]
     r = li[mid:]
     
-    l,inv1 = mergeSort(l,inv)
-    r,inv2 = mergeSort(r,inv)
+    l,inv1 = inversions(l,inv)
+    r,inv2 = inversions(r,inv)
     
     return merge(l,r,inv1+inv2)
-    
     
 def merge(l,r,inv):
     
@@ -69,8 +81,6 @@ def merge(l,r,inv):
     return m,inv
 
 
-        
-    
 if __name__ == "__main__":
     
     li = [3,1,4,2]
@@ -79,7 +89,10 @@ if __name__ == "__main__":
     # >> 3
     li3 = [1,20,6,4,5]
     # >> 5
+    li4 = [6,5,4,3,2,1]
+    # >> 15 
     
-    print mergeSort(li)
-    print mergeSort(li2)
-    print mergeSort(li3)
+    print inversions(li)
+    print inversions(li2)
+    print inversions(li3)
+    print inversions(li4)
